ac06b235645a383f8fea84fec853804774c84b98
Yooodh <eogus2604@hanmail.net>
2022-06-02 08:29:15 +0900

    v0.4

--- CodingAngma/src/component/Hello.js
+++ CodingAngma/src/component/Hello.js
@@ -1,18 +1,23 @@
+import World from "./World";
+import styles from "./Hello.module.css";
+
 // // const Hello = function () {
 // const Hello = () => {
 //     <p>Hello</p>
 // };
-
-import World from "./World";
 
 // export default Hello;
 
 export default function Hello() {
     return (
         <div>
-            <h1>Hello</h1>
-            <World />
-            <World />
+            <h1 style={{
+                    color : '#f00',
+                    borderRight : '2px solid #000',
+                    marginBottom : '30px',
+                    opacity : 1,
+            }}>Hello</h1>
+            <div className={styles.box} >Hello</div>
         </div>
     );
 }
