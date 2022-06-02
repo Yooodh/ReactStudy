05be71150d6bc55eb48ab06fa2809c8d34114dac
Yooodh <eogus2604@hanmail.net>
2022-06-03 08:06:43 +0900

    v0.7

--- CodingAngma/src/Exercise.js
+++ CodingAngma/src/Exercise.js
@@ -9,7 +9,7 @@ import Hello from './component/Hello';
 
 function Exercise() {
 
-    const name = "Tom";
+    const name0 = "Tom";
     // const user = {
     //   name: "Jane"
     // }
@@ -35,6 +35,24 @@ function Exercise() {
         console.log(txt);
     }
 
+        // let name = "Mike";
+
+    // 배열의 첫 번째 값은 state(변수명)이고 두 번째 값은 state를 변경해주는 함수
+    // setName 함수가 호출 되어서 name이 바뀌면 리액트는 이 컴포넌트를 다시 랜더링 해준다.
+    // useState() 괄호 안에는 초기 값이 들어간다.
+    // 동일한 컴포넌트라도 state는 각각 관리 된다. 다른 state에 영향을 미치지 않는다.
+    const [name, setName] = useState('Mike');
+
+    // Mike일 때는 Jane으로 Jane일 때는 Mike로 바꿔준다.
+    // function changeName() {
+    //     // name = name === "Mike" ? "Jane" : "Mike";
+    //     // const newName = name === "Mike" ? "Jane" : "Mike";
+
+    //     // document.getElementById("name").innerText = name;
+    //     // setName(newName);
+    //     setName(name === "Mike" ? "Jane" : "Mike");
+    // }
+
     return (
         // class는 자바스크립트의 예약어 이기 때문에 className으로 쓴다.
         <div className="App">
@@ -48,7 +66,7 @@ function Exercise() {
         }}
         >
             {/* 중괄호 내부에서 변수를 사용할 수 있다. */}
-            Hello, {name}.<p>{2 + 3}</p> {/* 숫자나 문자는 잘 나오지만 boolean 타입이나 객체는 표현하지 못한다. */}
+            Hello, {name0}.<p>{2 + 3}</p> {/* 숫자나 문자는 잘 나오지만 boolean 타입이나 객체는 표현하지 못한다. */}
             {/* <p>{user}</p>  에러 발생 */}
         </h1>
         <a href={naver.url}>{naver.name}</a>
@@ -86,6 +104,11 @@ function Exercise() {
             }}
             />
         </div>
+
+        <h2 id="name">{name}</h2>
+            <button onClick={() => {
+                setName(name === "Mike" ? "Jane" : "Mike")
+            }}>Change</button>
 
         </div>
     );
--- CodingAngma/src/component/Hello.js
+++ CodingAngma/src/component/Hello.js
@@ -1,31 +1,25 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello() {
+export default function Hello({age}) {
 
-    // let name = "Mike";
-
-    // 배열의 첫 번째 값은 state(변수명)이고 두 번째 값은 state를 변경해주는 함수
-    // setName 함수가 호출 되어서 name이 바뀌면 리액트는 이 컴포넌트를 다시 랜더링 해준다.
-    // useState() 괄호 안에는 초기 값이 들어간다.
-    // 동일한 컴포넌트라도 state는 각각 관리 된다. 다른 state에 영향을 미치지 않는다.
     const [name, setName] = useState('Mike');
 
-    // Mike일 때는 Jane으로 Jane일 때는 Mike로 바꿔준다.
-    // function changeName() {
-    //     // name = name === "Mike" ? "Jane" : "Mike";
-    //     // const newName = name === "Mike" ? "Jane" : "Mike";
-
-    //     // document.getElementById("name").innerText = name;
-    //     // setName(newName);
-    //     setName(name === "Mike" ? "Jane" : "Mike");
-    // }
+    const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
 
     return (
         <div>
-            <h2 id="name">{name}</h2>
-            <button onClick={() => {
-                setName(name === "Mike" ? "Jane" : "Mike")
-            }}>Change</button>
+            <h2 id="name">
+                {name}({age}) : {msg}
+            </h2>
+            <UserName name ={name} />
+            <button 
+                onClick={() => {
+                    setName(name === "Mike" ? "Jane" : "Mike")
+                }}
+            >
+                Change
+            </button>
         </div>
     );
 }
