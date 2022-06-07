7576ff964fbef08ce0425886578779a5b24858a4
Yooodh <eogus2604@hanmail.net>
2022-06-08 07:15:50 +0900

    v1.4

--- CodingAngma/src/component/Header.js
+++ CodingAngma/src/component/Header.js
@@ -7,13 +7,13 @@ export default function Header() {
         </h1>
 
         <div className="menu">
-            <a href="#x" className="link">
+            <Link to="/create_word" className="link">
                 단어 추가
-            </a>
+            </Link>
 
-            <a href="#x" className="link">
+            <Link to="/create_day" className="link">
                 Day 추가
-            </a>
+            </Link>
         </div>
 
     </div>
