b4916c9b748b38820dba4cac2c17ae7161c3484f
Yooodh <eogus2604@hanmail.net>
2023-06-09 00:35:03 +0900

    v0.3

---
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  const onSubmit = () => {
+    alert('submitted');
+  };
+
+  const onKeyUp = (event) => {
+    if (event.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className='App'>
+      <input onKeyUp={onKeyUp} />
+      <button onClick={onSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
