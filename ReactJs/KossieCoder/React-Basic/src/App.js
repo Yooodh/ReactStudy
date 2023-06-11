9390c2c72e80119650f07728f4c9a48b2a538662
Yooodh <eogus2604@hanmail.net>
2023-06-12 00:25:02 +0900

    v0.6

--- ReactJs/KossieCoder/React-Basic/src/App.js
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [username, setUsername] = useState('');
@@ -8,8 +8,26 @@ function App() {
     console.log(username, password);
   };
 
+  const [count, setCount] = useState(0);
+  const [kossie, setKossie] = useState(0);
+  useEffect(() => {
+    console.log(count);
+  }, [count, kossie]);
+
+  useEffect(() => {
+    console.log('first rendering');
+  }, []);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className='App'>
+      <h1>Kossie Coder</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setKossie(kossie + 1)}>Click1</button>
+
       <form onSubmit={onSubmit}>
         <input
           placeholder='Username'
