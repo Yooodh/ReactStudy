7a0cb6457c791522d4aae6229de245a10a416ed8
Yooodh <eogus2604@hanmail.net>
2023-06-11 23:17:21 +0900

    v0.5

--- ReactJs/KossieCoder/React-Basic/src/App.js
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -1,35 +1,30 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [text, setText] = useState('Kossie');
-  const onSubmit = () => {
-    alert('submitted');
-  };
-
-  const onKeyUp = (event) => {
-    if (event.keyCode === 13) {
-      onSubmit();
-    }
-  };
-
-  // let text = 'Kossie';
-
-  const updateText = () => {
-    // text = 'Coder';
-    setText('Coder');
-    console.log(text);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
   };
 
   return (
     <div className='App'>
-      <input onKeyUp={onKeyUp} />
-      <button onClick={onSubmit}>Submit</button>
-
-      <br />
-      <br />
-
-      <span>{text}</span>
-      <button onClick={updateText}>Update</button>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder='Username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type='submit'>Update</button>
+      </form>
     </div>
   );
 }
