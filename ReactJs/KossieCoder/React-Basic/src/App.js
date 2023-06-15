942db6e0c22fc7168212cbb9c33b5c2086fcdb04
Yooodh <eogus2604@hanmail.net>
2023-06-15 16:52:29 +0900

    v0.9

--- ReactJs/KossieCoder/React-Basic/src/App.js
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -4,12 +4,21 @@ import KossieCounter from './components/KossieCounter';
 import Login from './components/Login';
 
 function App() {
+  const [buttonName, setButtonName] = useState('클릭');
+  const clickButton = () => {
+    setButtonName('click');
+  };
+
   return (
     <div className='App'>
       <h1>Kossie Coder</h1>
+      <Counter click='click1' />
+      <Counter click={buttonName} />
       <Counter />
-      <Counter />
-      <Counter />
+      <button onClick={clickButton}>Click</button>
+
+      <br />
+
       <KossieCounter />
       <Login />
     </div>
--- ReactJs/KossieCoder/React-Basic/src/components/Counter.js
+++ ReactJs/KossieCoder/React-Basic/src/components/Counter.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
-const Counter = () => {
+const Counter = (props) => {
   const [count, setCount] = useState(0);
   const [kossie, setKossie] = useState(0);
 
   const increment = () => {
     setCount(count + 2);
   };
+  const clickString = props.click || 'Click';
 
   return (
     <>
-      <button onClick={increment}>Click {count}</button>
+      <button onClick={increment}>
+        {clickString} {count}
+      </button>
     </>
   );
 };
