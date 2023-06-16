7638b882bfde874b5cc2daab1c7e559cd768716f
Yooodh <eogus2604@hanmail.net>
2023-06-16 14:07:21 +0900

    v1.0

--- ReactJs/KossieCoder/React-Basic/src/App.js
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -9,17 +9,34 @@ function App() {
     setButtonName('click');
   };
 
+  const [condition, setCondition] = useState(false);
+  const toggle = () => setCondition(!condition);
+  useEffect(() => {
+    console.log(condition);
+  }, [condition]);
+
+  const renderCondition = condition ? 'True' : 'False';
   return (
     <div className='App'>
       <h1>Kossie Coder</h1>
+      <div>{renderCondition}</div>
+      <button onClick={toggle}>Toggle</button>
+
+      <br />
+      <br />
+
       <Counter click='click1' />
       <Counter click={buttonName} />
       <Counter />
       <button onClick={clickButton}>Click</button>
 
       <br />
+      <br />
 
       <KossieCounter />
+
+      <br />
+      <br />
       <Login />
     </div>
   );
