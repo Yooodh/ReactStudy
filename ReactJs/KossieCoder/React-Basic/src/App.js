c01c1a9a3d19c0ccd891e802cbaebf7d45c103bb
Yooodh <eogus2604@hanmail.net>
2023-06-21 03:59:29 +0900

    v1.2

--- ReactJs/KossieCoder/React-Basic/src/App.js
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import Counter from './components/Counter';
-import KossieCounter from './components/KossieCounter';
-import Login from './components/Login';
+// import React, { useEffect, useState } from 'react';
+// // import Counter from './components/Counter';
+// // import KossieCounter from './components/KossieCounter';
+// // import Login from './components/Login';
+// // import Toggle from './components/Toggle';
 import Movie from './components/Movie';
-import Toggle from './components/Toggle';
 
 function App() {
-  const [buttonName, setButtonName] = useState('클릭');
+  // const [buttonName, setButtonName] = useState('클릭');
+
+  const movies = [
+    { title: 'kossie coder1', year: 2001 },
+    { title: 'kossie coder2', year: 2002 },
+    { title: 'kossie coder3', year: 2003 },
+    { title: 'kossie coder4', year: 2004 },
+  ];
+
+  const renderMovies = movies.map((movie) => {
+    return <Movie movie={movie} key={movie.title} />;
+  });
 
   return (
     <div className='App'>
-      <h1>Kossie Coder</h1>
+      {/* <h1>Kossie Coder</h1>
       <Toggle />
 
       <br />
@@ -27,9 +38,9 @@ function App() {
 
       <br />
       <br />
-      <Login />
-
-      <Movie />
+      <Login /> */}
+      <h1>Movie list</h1>
+      {renderMovies}
     </div>
   );
 }
--- ReactJs/KossieCoder/React-Basic/src/components/Movie.js
+++ ReactJs/KossieCoder/React-Basic/src/components/Movie.js
@@ -1,24 +1,8 @@
-const Movie = () => {
-  const movies = [
-    { title: 'kossie coder1', year: 2001 },
-    { title: 'kossie coder2', year: 2002 },
-    { title: 'kossie coder3', year: 2003 },
-    { title: 'kossie coder4', year: 2004 },
-  ];
-
-  const renderMovies = movies.map((movie) => {
-    return (
-      <div className='movie' key={movie.title}>
-        <div className='movie-title'>{movie.title}</div>
-        <div className='movie-year'>{movie.year}</div>
-      </div>
-    );
-  });
-
+const Movie = ({ movie }) => {
   return (
-    <div>
-      <h1>Movie list</h1>
-      {renderMovies}
+    <div className='movie'>
+      <div className='movie-title'>{movie.title}</div>
+      <div className='movie-year'>{movie.year}</div>
     </div>
   );
 };
