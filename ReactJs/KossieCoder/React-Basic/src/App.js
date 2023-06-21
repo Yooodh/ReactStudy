47f49e8f67ad521f8a988ba7dca6a25a096c6162
Yooodh <eogus2604@hanmail.net>
2023-06-22 03:24:26 +0900

    v1.3

--- ReactJs/KossieCoder/React-Basic/src/App.js
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -3,21 +3,38 @@
 // // import KossieCounter from './components/KossieCounter';
 // // import Login from './components/Login';
 // // import Toggle from './components/Toggle';
+import { useEffect, useState } from 'react';
 import Movie from './components/Movie';
 
 function App() {
   // const [buttonName, setButtonName] = useState('클릭');
-
-  const movies = [
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const [movies, setMovies] = useState([
     { title: 'kossie coder1', year: 2001 },
     { title: 'kossie coder2', year: 2002 },
     { title: 'kossie coder3', year: 2003 },
     { title: 'kossie coder4', year: 2004 },
-  ];
+  ]);
+
+  useEffect(() => {
+    console.log('render');
+  });
 
   const renderMovies = movies.map((movie) => {
     return <Movie movie={movie} key={movie.title} />;
   });
+
+  const addMovie = (event) => {
+    event.preventDefault();
+    setMovies([
+      ...movies,
+      {
+        title: movieTitle,
+        year: movieYear,
+      },
+    ]);
+  };
 
   return (
     <div className='App'>
@@ -40,6 +57,23 @@ function App() {
       <br />
       <Login /> */}
       <h1>Movie list</h1>
+      <form onSubmit={addMovie}>
+        <input
+          tupe='text'
+          value={movieTitle}
+          placeholder='영화제목'
+          onChange={(e) => setMovieTitle(e.target.value)}
+        ></input>
+        <br />
+        <input
+          tupe='text'
+          value={movieYear}
+          placeholder='개봉년도'
+          onChange={(e) => setMovieYear(e.target.value)}
+        ></input>
+        <br />
+        <button type='submit'>영화추가</button>
+      </form>
       {renderMovies}
     </div>
   );
--- ReactJs/KossieCoder/my-app/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className='App'>Kossie Coder</div>;
-}
-
-export default App;
