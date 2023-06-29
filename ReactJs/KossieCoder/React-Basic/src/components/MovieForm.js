c54efd8936d54f18c4ae28491fe2b8b73a66c47c
Yooodh <eogus2604@hanmail.net>
2023-06-29 15:52:33 +0900

    v1.7

--- ReactJs/KossieCoder/React-Basic/src/components/MovieForm.js
+++ ReactJs/KossieCoder/React-Basic/src/components/MovieForm.js
@@ -4,20 +4,45 @@ const MovieForm = ({ addMovie }) => {
   const [movieTitle, setMovieTitle] = useState('');
   const [movieYear, setMovieYear] = useState('');
 
+  const [titleError, settitleError] = useState('');
+  const [yearError, setyearError] = useState('');
+
   const resetForm = () => {
     setMovieTitle('');
     setMovieYear('');
   };
 
+  const validateForm = () => {
+    resetErrors();
+    let validated = true;
+    if (!movieTitle) {
+      settitleError('영화제목을 넣어주세요');
+      validated = false;
+    }
+
+    if (!movieYear) {
+      setyearError('개봉년도를 넣어주세요');
+      validated = false;
+    }
+    return validated;
+  };
+
+  const resetErrors = () => {
+    settitleError('');
+    setyearError('');
+  };
+
   const onSubmit = (event) => {
     event.preventDefault();
-    addMovie({
-      id: Date.now(),
-      title: movieTitle,
-      year: movieYear,
-    });
-
-    resetForm();
+    if (validateForm()) {
+      addMovie({
+        id: Date.now(),
+        title: movieTitle,
+        year: movieYear,
+      });
+      resetErrors();
+      resetForm();
+    }
   };
   return (
     <form onSubmit={onSubmit}>
@@ -28,13 +53,15 @@ const MovieForm = ({ addMovie }) => {
         onChange={(e) => setMovieTitle(e.target.value)}
       ></input>
       <br />
+      <div style={{ color: 'red' }}>{titleError}</div>
       <input
-        tupe='text'
+        tupe='number'
         value={movieYear}
         placeholder='개봉년도'
         onChange={(e) => setMovieYear(e.target.value)}
       ></input>
       <br />
+      <div style={{ color: 'red' }}>{yearError}</div>
       <button type='submit'>영화추가</button>
     </form>
   );
