6bf51fb1ffef2bef7fb0a7fb4546eb0277aba015
Yooodh <eogus2604@hanmail.net>
2023-07-04 23:53:33 +0900

    v2.1

--- ReactJs/KossieCoder/React-Basic/src/App.js
+++ ReactJs/KossieCoder/React-Basic/src/App.js
@@ -1,16 +1,9 @@
-// import React, { useEffect, useState } from 'react';
-// // import Counter from './components/Counter';
-// // import KossieCounter from './components/KossieCounter';
-// // import Login from './components/Login';
-// // import Toggle from './components/Toggle';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Movie from './components/Movie';
 import MovieForm from './components/MovieForm';
 import Navbar from './components/Navbar';
 
 function App() {
-  // const [buttonName, setButtonName] = useState('클릭');
-
   const [movies, setMovies] = useState([]);
 
   const removeMovie = (id) => {
@@ -37,24 +30,6 @@ function App() {
 
   return (
     <div className='App'>
-      {/* <h1>Kossie Coder</h1>
-      <Toggle />
-
-      <br />
-      <br />
-
-      <Counter click='click1' />
-      <Counter click={buttonName} />
-      <Counter />
-
-      <br />
-      <br />
-
-      <KossieCounter />
-
-      <br />
-      <br />
-      <Login /> */}
       <Navbar />
       <h1>Movie list</h1>
       <MovieForm addMovie={addMovie} />
