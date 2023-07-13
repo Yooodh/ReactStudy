799d9321992fe05daff1e2641ba4c7aafedc81be
Yooodh <eogus2604@hanmail.net>
2023-07-13 15:07:26 +0900

    v2.8

--- ReactJs/KossieCoder/React-Basic/src/pages/Users.js
+++ ReactJs/KossieCoder/React-Basic/src/pages/Users.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import UserList from '../components/UserList';
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
-      console.log(response);
+      setUsers(response.data);
     });
   });
 
-  return <h1>Users</h1>;
+  return (
+    <>
+      <h1>Users</h1>
+      <UserList users={users} />
+    </>
+  );
 };
 
 export default Users;
