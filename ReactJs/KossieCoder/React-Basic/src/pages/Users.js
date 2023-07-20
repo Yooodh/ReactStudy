6de83d477a2025335a6d911b145e9e19069c65f6
Yooodh <eogus2604@hanmail.net>
2023-07-20 14:25:54 +0900

    v3.4

--- ReactJs/KossieCoder/React-Basic/src/pages/Users.js
+++ ReactJs/KossieCoder/React-Basic/src/pages/Users.js
@@ -2,27 +2,22 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import UserList from '../components/UserList';
 import Spinner from '../components/Spinner';
-import { useParams } from 'react-router-dom';
 
 const Users = () => {
-  const [user, setUser] = useState(null);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { id } = useParams();
-  console.log(user);
 
   useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/users' + id)
-      .then((response) => {
-        setUser(response.data);
-        setLoading(false);
-      });
+    axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+      setUsers(response.data);
+      setLoading(false);
+    });
   });
 
   return (
     <>
       <h1>Users</h1>
-      {loading ? <Spinner /> : null}
+      {loading ? <Spinner /> : <UserList users={users} />}
     </>
   );
 };
