48bb096cf956a256a6106ed457d973c3f6715d3b
Yooodh <eogus2604@hanmail.net>
2023-08-02 23:53:23 +0900

    v0.7

--- ReactJs/KossieCoder/react-basic2/src/App.js
+++ ReactJs/KossieCoder/react-basic2/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  const printHello = () => {
-    console.log('hello');
+  const [number, setNumber] = useState(1);
+  const [number1, setNumber1] = useState(1);
+  const [number2, setNumber2] = useState(1);
+
+  const double = () => {
+    const doubleNumber = number * 2;
+    setNumber(number * 2);
+    setNumber1(number1 * 2);
+    setNumber2(doubleNumber);
+    console.log(number);
+    console.log(number1);
+    console.log(doubleNumber);
   };
 
   return (
     <>
-      <button onClick={printHello}>Submit</button>
+      <div>{number}</div>
+      <div>{number1}</div>
+      <div>{number2}</div>
+      <button onClick={double}>Submit</button>
     </>
   );
 }
