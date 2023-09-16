bf8546cbbd0ae18739054771a873f8f25fcff7f9
Yooodh <eogus2604@hanmail.net>
2023-09-17 00:42:46 +0900

    v0.8

--- ReactJs/DBN/management/src/App.js
+++ ReactJs/DBN/management/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from '@mui/material';
 
 const customers = [
   {
     id: 1,
     image: 'https://placeimg.com/64/64/1',
-    name: '홍길동',
-    birthday: '961222',
-    gender: '남자',
-    job: '대학생',
-  },
-  {
-    id: 2,
-    image: 'https://placeimg.com/64/64/2',
     name: '유대현',
     birthday: '960112',
     gender: '남자',
     job: '프로그래머',
   },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: '홍길동',
+    birthday: '961222',
+    gender: '남자',
+    job: '대학생',
+  },
+
   {
     id: 3,
     image: 'https://placeimg.com/64/64/3',
@@ -33,18 +41,32 @@ class App extends Component {
   render() {
     return (
       <div>
-        {customers.map((c) => {
-          return (
-            <Customer
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        })}
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => {
+              return (
+                <Customer
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
       </div>
     );
   }
