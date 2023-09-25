c3627681406223dce871ee917c23dd4f2d3d1509
Yooodh <eogus2604@hanmail.net>
2023-09-25 14:40:21 +0900

    v0.4

--- ReactJs/TutorialsDev/emart/src/component/Navbar.jsx
+++ ReactJs/TutorialsDev/emart/src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div>
-      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <nav className='navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm'>
         <div className='container'>
           <a className='navbar-brand fw-bold fs-4' href='#'>
             LA COLLECTION
