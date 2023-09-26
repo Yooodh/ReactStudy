dcec7a414f6e66024fc5fa2c3dac5535482e81f7
Yooodh <eogus2604@hanmail.net>
2023-09-27 03:01:18 +0900

    v0.6

--- ReactJs/TutorialsDev/emart/src/component/Products.jsx
+++ ReactJs/TutorialsDev/emart/src/component/Products.jsx
@@ -39,6 +39,31 @@ const Products = () => {
           <button className='btn btn-outline-dark me-2'>Jewelery</button>
           <button className='btn btn-outline-dark me-2'>Electronic</button>
         </div>
+        {filter.map((product) => {
+          return (
+            <>
+              <div className='col-md-3 mb-4'>
+                <div className='card h-100 text-center p-4' key={product.id}>
+                  <img
+                    src={product.image}
+                    className='card-img-top'
+                    alt={product.title}
+                    height='250px'
+                  />
+                  <div className='card-body'>
+                    <h5 className='card-title mb-0'>
+                      {product.title.substring(0, 12)}...
+                    </h5>
+                    <p className='card-text lead fw-bold'>${product.price}</p>
+                    <a href='#' className='btn btn-outline-dark'>
+                      Buy Now
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </>
     );
   };
