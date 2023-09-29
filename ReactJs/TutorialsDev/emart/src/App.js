12f84c40a873094d74f2473720aba898fb0d9649
Yooodh <eogus2604@hanmail.net>
2023-09-29 12:32:55 +0900

    v0.8

--- ReactJs/TutorialsDev/emart/src/App.js
+++ ReactJs/TutorialsDev/emart/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Home from './component/Home';
+import { Routes, Route } from 'react-router-dom';
+import Products from './component/Products';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='products' element={<Products />} />
+        <Home />
+      </Routes>
     </>
   );
 }
