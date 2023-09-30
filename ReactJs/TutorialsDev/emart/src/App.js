33269da270c769eb0d3c50cd59aa2c11dd86e33e
Yooodh <eogus2604@hanmail.net>
2023-09-30 13:45:21 +0900

    v0.9

--- ReactJs/TutorialsDev/emart/src/App.js
+++ ReactJs/TutorialsDev/emart/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='products' element={<Products />} />
-        <Home />
+        {/* <Home /> */}
       </Routes>
     </>
   );
