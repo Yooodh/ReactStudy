35a1efd3847a541b75cef11493128b0ffd467a8f
Yooodh <eogus2604@hanmail.net>
2023-10-18 00:48:09 +0900

    v0.4

--- ReactJs/TutorialsDev/Ecommerce App/src/App.js
+++ ReactJs/TutorialsDev/Ecommerce App/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './modules/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ ReactJs/TutorialsDev/Ecommerce App/src/modules/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hero from '../../components/Header/Hero';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+export default Home;
