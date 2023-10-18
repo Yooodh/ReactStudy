21eefc8efdf7127726e550ba399e3944846dffe7
Yooodh <eogus2604@hanmail.net>
2023-10-19 00:37:10 +0900

    v0.5

--- ReactJs/TutorialsDev/Ecommerce App/src/modules/Home/index.js
+++ ReactJs/TutorialsDev/Ecommerce App/src/modules/Home/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hero from '../../components/Header/Hero';
+import Products from '../../components/Header/Products';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <h2>Products</h2>
+      <Products />
     </>
   );
 };
