0ce10a6cc4670cea79674d4f4d02a32a69aef838
Yooodh <eogus2604@hanmail.net>
2023-10-28 23:03:16 +0900

    v1.2

--- ReactJs/TutorialsDev/Ecommerce App/src/App.js
+++ ReactJs/TutorialsDev/Ecommerce App/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Home from './modules/Home';
 import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
-import Product from './modules/Home/Product';
+import Product from './modules/Product';
 
 function App() {
   return (
