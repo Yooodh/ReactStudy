e12f48d7df12559d2020ceea5c0859318296de51
Yooodh <eogus2604@hanmail.net>
2023-11-04 00:02:04 +0900

    Search Weather

---
+++ ReactJs/TutorialsDev/search_weather/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Searchweather from './component/SearchWeather';
+
+function App() {
+  return (
+    <div>
+      <Searchweather />
+    </div>
+  );
+}
+
+export default App;
