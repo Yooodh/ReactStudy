98b2df77c6c4c38b17d42df7718216eacfb4ba42
Yooodh <eogus2604@hanmail.net>
2023-11-09 16:12:19 +0900

    Create Home

--- ReactJs/TutorialsDev/website/src/Component/Home.jsx
+++ ReactJs/TutorialsDev/website/src/Component/Home.jsx
@@ -1,5 +1,45 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className='container my-5 py-4'>
+        <div className='row'>
+          <div className='col-md-5 mt-5'>
+            <h1 className='display-5 mb-4'>
+              I'm Web Developer <br /> Yoo
+            </h1>
+            <p className='lead mb-4'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Laboriosam iure odio, pariatur ipsam commodi rem nesciunt error
+              fugit atque facilis laudantium ea quo consequatur vitae,
+              necessitatibus ullam delectus incidunt. Eligendi!
+            </p>
+            <Link
+              className='btn btn-outline-primary me-4'
+              to='/contact'
+              role='button'
+            >
+              Get Started
+            </Link>
+            <Link className='btn btn-primary' to='/contact' role='button'>
+              More About me
+            </Link>
+          </div>
+          {/* For Image */}
+          <div className='col-md-7 d-flex justify-content-center align-items-center'>
+            <img
+              className='mt-5'
+              src='/assets/images/User.png'
+              alt='Yoo'
+              height='350px'
+              width='300px'
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
