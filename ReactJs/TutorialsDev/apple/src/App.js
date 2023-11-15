da3e97c17e44bec566f0d7b4937c2bc1096b7df3
Yooodh <eogus2604@hanmail.net>
2023-11-15 21:35:21 +0900

    v0.5

--- ReactJs/TutorialsDev/apple/src/App.js
+++ ReactJs/TutorialsDev/apple/src/App.js
@@ -7,6 +7,7 @@ import Product from './components/Product';
 import Contact from './components/Contact';
 // import { Navigate, Route, Routes } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         {/* <Route path='/' element={<Navigate replace to='/Home' />} /> */}
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<Product />} />
+        <Route path='/products/:id' element={<ProductDetail />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
