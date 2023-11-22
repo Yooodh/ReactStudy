bb5482cbd30159091fe9844e56e8bbd3e53de303
Yooodh <eogus2604@hanmail.net>
2023-11-23 00:59:30 +0900

    v0.3

--- ReactJs/TechinfoYT/resturant-website/src/App.js
+++ ReactJs/TechinfoYT/resturant-website/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Menu from './pages/Menu';
+import Pagenotfound from './pages/Pagenotfound';
 
 function App() {
   return (
     <div>
-      <h1>Resturant Website</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='*' element={<Pagenotfound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
