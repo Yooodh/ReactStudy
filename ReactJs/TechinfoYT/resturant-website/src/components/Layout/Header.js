c740763e4a817d0fe3e36b17c5d6ed7d8b72c6cd
Yooodh <eogus2604@hanmail.net>
2023-11-24 00:02:29 +0900

    v0.4

--- ReactJs/TechinfoYT/resturant-website/src/components/Layout/Header.js
+++ ReactJs/TechinfoYT/resturant-website/src/components/Layout/Header.js
@@ -1,9 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Divider,
+  Typography,
+} from '@mui/material';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import '../../styles/HeaderStyles.css';
 
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  // handle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  // menu drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography
+        color={'goldenrod'}
+        variant='h6'
+        component='div'
+        sx={{ flexGrow: 1, my: 2 }}
+      >
+        <FastfoodIcon />
+        My Resturant
+      </Typography>
+      <Divider />
+      <ul className='mobile-navigation'>
+        <li>
+          <Link activeClassName='active' to={'/'}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to={'/menu'}>Menu</Link>
+        </li>
+        <li>
+          <Link to={'/about'}>About</Link>
+        </li>
+        <li>
+          <Link to={'/contact'}>Contact</Link>
+        </li>
+      </ul>
+    </Box>
+  );
   return (
     <>
-      <h1>Navabr</h1>
+      <Box>
+        <AppBar component={'nav'} sx={{ bgcolor: 'black' }}>
+          <Toolbar>
+            <IconButton
+              color='inherit'
+              aria-label='open drawer'
+              edge='start'
+              sx={{
+                mr: 2,
+                display: { sm: 'none' },
+              }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={'goldenrod'}
+              variant='h6'
+              component='div'
+              sx={{ flexGrow: 1 }}
+            >
+              <FastfoodIcon />
+              My Resturant
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <ul className='navigation-menu'>
+                <li>
+                  <Link activeClassName='active' to={'/'}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'/menu'}>Menu</Link>
+                </li>
+                <li>
+                  <Link to={'/about'}>About</Link>
+                </li>
+                <li>
+                  <Link to={'/contact'}>Contact</Link>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component='nav'>
+          <Drawer
+            variant='temporary'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: '240px',
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
+      </Box>
     </>
   );
 };
