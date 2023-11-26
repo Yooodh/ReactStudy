9d3ccc786eddec01e55adefe071d72f67cdfc74b
Yooodh <eogus2604@hanmail.net>
2023-11-27 01:17:46 +0900

    v0.5

---
+++ ReactJs/TechinfoYT/resturant-website/src/components/Layout/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <>
+      <Box
+        sx={{ textAlign: 'center', bgcolor: '#1A1A19', color: 'white', p: 3 }}
+      >
+        <Typography
+          variant='h5'
+          sx={{
+            '@media (max-width:600px)': {
+              fontSize: '1rem',
+            },
+          }}
+        >
+          All Rights Reserved &copy; Techinfo YT
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+export default Footer;
