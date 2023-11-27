b8d23e04ce7b3bbe4a7303aa55876412ee842033
Yooodh <eogus2604@hanmail.net>
2023-11-28 00:21:38 +0900

    v0.6

--- ReactJs/TechinfoYT/resturant-website/src/components/Layout/Footer.js
+++ ReactJs/TechinfoYT/resturant-website/src/components/Layout/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Box, Typography } from '@mui/material';
 
 const Footer = () => {
@@ -7,6 +11,27 @@ const Footer = () => {
       <Box
         sx={{ textAlign: 'center', bgcolor: '#1A1A19', color: 'white', p: 3 }}
       >
+        <Box
+          sx={{
+            my: 3,
+            '& svg': {
+              fontSize: '60px',
+              cursor: 'pointer',
+              mr: 2,
+            },
+            '& svg:hover': {
+              color: 'goldenrod',
+              transform: 'translateX(5px)',
+              transition: 'all 400ms',
+            },
+          }}
+        >
+          {/* Icons */}
+          <InstagramIcon />
+          <TwitterIcon />
+          <GitHubIcon />
+          <YouTubeIcon />
+        </Box>
         <Typography
           variant='h5'
           sx={{
