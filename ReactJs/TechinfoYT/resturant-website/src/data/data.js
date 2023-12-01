e8dd2b64e1ce717acd2aaef1330b89bc7e5c6508
Yooodh <eogus2604@hanmail.net>
2023-12-02 01:17:10 +0900

    v0.9

---
+++ ReactJs/TechinfoYT/resturant-website/src/data/data.js
@@ -0,0 +1,51 @@
+import Dosa from '../images/dosa.jpg';
+import Cholda from '../images/chhola.jpg';
+import Idli from '../images/idli.jpg';
+import MadalaDosa from '../images/masala.jpg';
+import Paneer from '../images/paneer.jpg';
+import Gujrati from '../images/gujrati.jpeg';
+
+export const MenuList = [
+  {
+    name: 'Dosa',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, quasi!',
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: 'Cholda',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, quasi!',
+    image: Cholda,
+    price: 250,
+  },
+  {
+    name: 'Idli',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, quasi!',
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: 'MadalaDosa',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, quasi!',
+    image: MadalaDosa,
+    price: 100,
+  },
+  {
+    name: 'Paneer',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, quasi!',
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: 'Gujrati',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, quasi!',
+    image: Gujrati,
+    price: 500,
+  },
+];
