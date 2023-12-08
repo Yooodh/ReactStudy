a35e04b38785a190d19fcef97e2a4c89ea395a3e
Yooodh <eogus2604@hanmail.net>
2023-12-09 01:04:41 +0900

    v0.5

--- ReactJs/HarshPathak/addtocart/src/components/Header.js
+++ ReactJs/HarshPathak/addtocart/src/components/Header.js
@@ -53,9 +53,29 @@ const Header = () => {
             'aria-labelledby': 'basic-button',
           }}
         >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={handleClose}>Logout</MenuItem>
+          <div
+            className='card_details d-flex justify-content-center align-items-center'
+            style={{ width: '24rem', padding: 10, position: 'relative' }}
+          >
+            <i
+              className='fas fa-close smallclose'
+              onClick={handleClose}
+              style={{
+                position: 'absolute',
+                top: 2,
+                right: 20,
+                fontSize: 23,
+                cursor: 'pointer',
+              }}
+            ></i>
+            <p style={{ fontSize: 22 }}>Your carts is empty</p>
+            <img
+              src='./cart.gif'
+              alt=''
+              className='emptycart_img'
+              style={{ width: '5rem', padding: 10 }}
+            />
+          </div>
         </Menu>
       </Navbar>
     </>
