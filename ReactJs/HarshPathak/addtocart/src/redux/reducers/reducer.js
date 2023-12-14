c81877ed447f28dcc36937e4826e66449211d4bb
Yooodh <eogus2604@hanmail.net>
2023-12-15 00:58:18 +0900

    v1.1

--- ReactJs/HarshPathak/addtocart/src/redux/reducers/reducer.js
+++ ReactJs/HarshPathak/addtocart/src/redux/reducers/reducer.js
@@ -5,10 +5,19 @@ const INIT_STATE = {
 export const cartreducer = (state = INIT_STATE, action) => {
   switch (action.type) {
     case 'ADD_CART':
-      return {
-        ...state,
-        carts: [...state.carts, action.payload],
-      };
+      const IteamIndex = state.carts.findIndex(
+        (iteam) => iteam.id === action.payload.id
+      );
+
+      if (IteamIndex >= 0) {
+        state.carts[IteamIndex].qnty += 1;
+      } else {
+        const temp = { ...action.payload, qnty: 1 };
+        return {
+          ...state,
+          carts: [...state.carts, temp],
+        };
+      }
 
     case 'RMV_CART':
       const data = state.carts.filter((el) => el.id !== action.payload);
