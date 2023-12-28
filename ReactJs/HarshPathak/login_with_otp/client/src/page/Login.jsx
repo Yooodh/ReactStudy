ceee7af7246587f4d9357910cea9b6342420ea92
Yooodh <eogus2604@hanmail.net>
2023-12-29 02:11:42 +0900

    v0.6

--- ReactJs/HarshPathak/login_with_otp/client/src/page/Login.jsx
+++ ReactJs/HarshPathak/login_with_otp/client/src/page/Login.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import '../styles/mix.css';
+import { NavLink } from 'react-router-dom';
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <section>
+        <div className='form_data'>
+          <div className='form_heading'>
+            <h1>Welcome Back, Log In</h1>
+            <p>Hi, we are you glad you are back. Please login.</p>
+          </div>
+          <form>
+            <div className='form_input'>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                name='email'
+                id=''
+                placeholder='Enter Your Email Address'
+              />
+            </div>
+            <button className='btn'>Login</button>
+            <p>
+              Don't have and account <NavLink to='/register'>Sign up</NavLink>
+            </p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Login;
