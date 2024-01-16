a6e5d345ec2538c6b543ae3d9b30a38e7c1f4969
Yooodh <eogus2604@hanmail.net>
2024-01-16 16:46:44 +0900

    v0.8

---
+++ ReactJs/NishaSingla/react-redux-toolkit/src/store/productSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const productSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {
+    fetchProducts(state, action) {
+      state.data = action.payload;
+    },
+  },
+});
+
+export const { add, remove } = productSlice.actions;
+export default productSlice.reducer;
+
+export function getProducts() {
+  return async function getProductsThunk(dispatch, getState) {
+    const data = await fetch('https://fakestoreapi.com/products');
+    const result = data.json();
+    dispatch(fetchProducts(result));
+  };
+}
