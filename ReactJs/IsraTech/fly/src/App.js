728a969343c2fd3d1147aa8759350888f8105ac3
Yooodh <eogus2604@hanmail.net>
2024-01-22 15:25:03 +0900

    v0.2

---
+++ ReactJs/IsraTech/fly/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './Component/Navbar/Navbar';
+import Home from './Component/Home/Home';
+import Search from './Component/Search/Search';
+import Support from './Component/Support/Support';
+import Info from './Component/Info/Info';
+import Lounge from './Component/Lounge/Lounge';
+import Travelers from './Component/Travelers/Travelers';
+import Subscribe from './Component/Subscribers/Subscribe';
+import Footer from './Component/Footer/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscribe />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
