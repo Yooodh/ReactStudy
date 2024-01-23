2a3890c4a7c241fe5ca9ccd775b6d6cfb623c626
Yooodh <eogus2604@hanmail.net>
2024-01-23 15:59:06 +0900

    v0.3

--- ReactJs/IsraTech/fly/src/Component/Navbar/Navbar.jsx
+++ ReactJs/IsraTech/fly/src/Component/Navbar/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
+// Imported Icons
+import { SiConsul } from 'react-icons/si';
+import { BsPhoneVibrate } from 'react-icons/bs';
+import { AiOutlineGlobal } from 'react-icons/ai';
+
+// Imported Images
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='NavBar flex'>
+      <div className='navBarOne flex'>
+        <div>
+          <SiConsul />
+        </div>
+        <div className='none flex'>
+          <li className='flex'>
+            <BsPhoneVibrate className='icon' /> Support
+          </li>
+          <li className='flex'>
+            <AiOutlineGlobal className='icon' /> Languages
+          </li>
+        </div>
+        <div className='atb flex'>
+          <span>Sign In</span>
+          <span>Sign Up</span>
+        </div>
+      </div>
+      <div className='navBartTwo'>
+        <div className='logoDiv'>
+          <img src='' alt='' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
