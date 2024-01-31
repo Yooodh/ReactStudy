9b2dd898e2fbee8a0b459b28d67f05f527728652
Yooodh <eogus2604@hanmail.net>
2024-01-31 13:28:37 +0900

    v1.0

--- ReactJs/IsraTech/fly/src/Component/Support/Support.jsx
+++ ReactJs/IsraTech/fly/src/Component/Support/Support.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Imported Images
+import gridImage from '../../assets/Images-Grid.png';
+
 const Support = () => {
   return (
     <div className='support container section'>
@@ -11,6 +14,39 @@ const Support = () => {
             Find help with booking and travel plans, see what to expect along
             the journey!
           </p>
+        </div>
+
+        <div className='infoDiv grid'>
+          <div className='textDiv grid'>
+            <div className='singleInfo'>
+              <span className='number'>01</span>
+              <h4>Travel requirements for Dubai</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+            <div className='singleInfo'>
+              <span className='number colorOne'>02</span>
+              <h4>Chauffeur services at your arrival</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+            <div className='singleInfo'>
+              <span className='number colorTwo'>03</span>
+              <h4>Multi-risk travel insurance</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+          </div>
+
+          <div className='imgDiv'>
+            <img src={gridImage} alt='' />
+          </div>
         </div>
       </div>
     </div>
