2d41141b2f57feb859bdef41bce56048417ff88f
Yooodh <eogus2604@hanmail.net>
2024-02-13 16:36:06 +0900

    v1.9

--- ReactJs/IsraTech/fly/src/Component/Home/Home.jsx
+++ ReactJs/IsraTech/fly/src/Component/Home/Home.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // Import Assests
 import video from '../../assets/videos.mp4';
 import aeroplane from '../../assets/takeOff.png';
 
+// Import AOS
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 const Home = () => {
+  // UseEffect to set animation duration
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <div className='home flex container'>
       <div className='mainText'>
-        <h1>Create Ever-lasting Memories With Us</h1>
+        <h1 data-aos='fade-up' data-aos-duration='2500'>
+          Create Ever-lasting Memories With Us
+        </h1>
       </div>
 
       <div className='homeImages flex'>
--- ReactJs/IsraTech/fly/src/Component/Support/Support.jsx
+++ ReactJs/IsraTech/fly/src/Component/Support/Support.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // Imported Images
 import gridImage from '../../assets/Images-Grid.png';
 
+// Import AOS
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 const Support = () => {
+  // UseEffect to set animation duration
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <div className='support container section'>
       <div className='sectionContainer'>
@@ -18,7 +27,11 @@ const Support = () => {
 
         <div className='infoDiv grid'>
           <div className='textDiv grid'>
-            <div className='singleInfo'>
+            <div
+              data-aos='fade-down'
+              data-aos-duration='2500'
+              className='singleInfo'
+            >
               <span className='number'>01</span>
               <h4>Travel requirements for Dubai</h4>
               <p>
@@ -26,7 +39,11 @@ const Support = () => {
                 along the journey to your favourite destinations!
               </p>
             </div>
-            <div className='singleInfo'>
+            <div
+              data-aos='fade-down'
+              data-aos-duration='3500'
+              className='singleInfo'
+            >
               <span className='number colorOne'>02</span>
               <h4>Chauffeur services at your arrival</h4>
               <p>
@@ -34,7 +51,11 @@ const Support = () => {
                 along the journey to your favourite destinations!
               </p>
             </div>
-            <div className='singleInfo'>
+            <div
+              data-aos='fade-down'
+              data-aos-duration='4500'
+              className='singleInfo'
+            >
               <span className='number colorTwo'>03</span>
               <h4>Multi-risk travel insurance</h4>
               <p>
@@ -44,8 +65,8 @@ const Support = () => {
             </div>
           </div>
 
-          <div className='imgDiv'>
-            <img src={gridImage} alt='' />
+          <div data-aos='fade-left' data-aos-duration='2500' className='imgDiv'>
+            <img src={gridImage} />
           </div>
         </div>
       </div>
