00e11fcd20b30a318fd579ccf0d3b1097e070476
Yooodh <eogus2604@hanmail.net>
2024-02-20 16:40:39 +0900

    v0.4

--- ReactJs/IsraTech/nike/src/components/Navbar.jsx
+++ ReactJs/IsraTech/nike/src/components/Navbar.jsx
@@ -9,28 +9,75 @@ const Navbar = () => {
     setDropdown(dropdown);
   };
   return (
-    <nav>
-      <div>
-        <div>
-          <div>
-            <span onClick={() => (window.location = '/')}>
+    <nav className='w-full h-24 flex flex-col justify-center items-center lg:bg-transparent bg-white lg:absolute fixed z-20'>
+      <div className='container mx-auto lg:px-6'>
+        <div className='lg:w-full w-11/12 mx-auto h-full flex justify-between xl:items-baseline items-center text-[#12222E]'>
+          <div className='flex flex-col gap-y-4'>
+            <span
+              onClick={() => (window.location = '/')}
+              className='flex items-center gap-x-2 font-bold text-2xl cursor-pointer'
+            >
               <img src='/nike-1ogo.svg' alt='nike logo' />
             </span>
           </div>
-          <ul>
-            <a href='#home'>Home</a>
-            <a href='#about'>About Us</a>
-            <a href='#products'>Products</a>
-            <a href='#contant-us'>Contant Us</a>
+          <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+            <a
+              href='#home'
+              className='font-montserrat leading-normal text-lg text-[#b2ab9f] no-underline'
+            >
+              Home
+            </a>
+            <a
+              href='#about'
+              className='font-montserrat leading-normal text-lg text-[#b2ab9f] no-underline'
+            >
+              About Us
+            </a>
+            <a
+              href='#products'
+              className='font-montserrat leading-normal text-lg text-[#b2ab9f] no-underline'
+            >
+              Products
+            </a>
+            <a
+              href='#contant-us'
+              className='font-montserrat leading-normal text-lg text-[#b2ab9f] no-underline'
+            >
+              Contant Us
+            </a>
           </ul>
           {dropdown ? (
-            <div onClick={showDropdown}>
+            <div
+              onClick={showDropdown}
+              className='lg:hidden text-[22px] cursor-pointer text-black'
+            >
               <MdClose />
             </div>
           ) : (
-            <HiMenuAlt3 />
+            <div
+              onClick={showDropdown}
+              className='lg:hidden text-[22px] cursor-pointer text-black'
+            >
+              <HiMenuAlt3 />
+            </div>
           )}
         </div>
+
+        {dropdown ? (
+          <div
+            onClick={showDropdown}
+            className='lg:hidden w-full h-[100vh] fixed top-24 bg-white transition ease-in-out duration-100'
+          >
+            <div className='w-full h-[320px] flex flex-col items-baseline pt-8 gap-4 text-[#12222E]'>
+              <ul>
+                <a href='#home'>Home</a>
+                <a href='#about'>About Us</a>
+                <a href='#products'>Products</a>
+                <a href='#contant-us'>Contant Us</a>
+              </ul>
+            </div>
+          </div>
+        ) : null}
       </div>
     </nav>
   );
