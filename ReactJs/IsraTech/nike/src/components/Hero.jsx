9a382a2f8545992d7aa23c64407ece3dd89fb5d4
Yooodh <eogus2604@hanmail.net>
2024-02-23 17:03:39 +0900

    v0.7

--- ReactJs/IsraTech/nike/src/components/Hero.jsx
+++ ReactJs/IsraTech/nike/src/components/Hero.jsx
@@ -55,7 +55,74 @@ const Hero = () => {
             </div>
           </div>
         </section>
-        <section></section>
+        <section className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-[#2f3233] bg-[url("/collection-background.svg")] bg-cover bg-center'>
+          <img
+            src={imageUrl}
+            alt=''
+            width='610'
+            height='500'
+            className='object-contain relative z-10 max-w-full h-auto'
+          />
+          <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+            <div>
+              <div
+                style={{
+                  borderColor: imageUrl === './shoe1.png' ? '#ff6452' : '',
+                }}
+                className='border-2 border-solid rounded-xl border-transparent cursor-pointer max-sm:flex-1'
+              >
+                <div className='flex justify-center items-center bg-[url("/blob.svg")] bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+                  <img
+                    src='/thumbnail-shoe1.svg'
+                    alt='thumbnail1-shoe1'
+                    width='127'
+                    height='103'
+                    className='object-contain max-w-full h-auto'
+                    onClick={() => setImageUrl('/shoe1.png')}
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div
+                style={{
+                  borderColor: imageUrl === './shoe2.png' ? '#ff6452' : '',
+                }}
+                className='border-2 border-solid rounded-xl border-transparent cursor-pointer max-sm:flex-1'
+              >
+                <div className='flex justify-center items-center bg-[url("/blob.svg")] bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+                  <img
+                    src='/thumbnail-shoe2.svg'
+                    alt='thumbnail2-shoe2'
+                    width='127'
+                    height='103'
+                    className='object-contain max-w-full h-auto'
+                    onClick={() => setImageUrl('/shoe2.png')}
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div
+                style={{
+                  borderColor: imageUrl === './shoe3.png' ? '#ff6452' : '',
+                }}
+                className='border-2 border-solid rounded-xl border-transparent cursor-pointer max-sm:flex-1'
+              >
+                <div className='flex justify-center items-center bg-[url("/blob.svg")] bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+                  <img
+                    src='/thumbnail-shoe3.svg'
+                    alt='thumbnail3-shoe3'
+                    width='127'
+                    height='103'
+                    className='object-contain max-w-full h-auto'
+                    onClick={() => setImageUrl('/shoe3.png')}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
