88dc97d31afdf377a9b19f6085029a443f82063e
Yooodh <eogus2604@hanmail.net>
2024-02-26 00:57:16 +0900

    v1.0

---
+++ ReactJs/IsraTech/nike/src/components/About_us.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const About_us = () => {
+  return (
+    <div id='about-us'>
+      <main>
+        <div>
+          <h1>
+            We provide You
+            <span> Super</span>
+            <span> Quality</span> Shoes
+          </h1>
+          <p>
+            Ensuring premium comfort and style, our meticulously crafted
+            footwear is designed to elevate your experience, providing you with
+            unmatched quality, innovation, and a touch of elegance.
+          </p>
+          <p>
+            Our dedication to detail and excellence ensures your satisfaction
+          </p>
+          <div>
+            <button>View details</button>
+          </div>
+          <div>
+            <img src='/shoe*.png' alt='shoe-8' width='570' height='522' />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default About_us;
--- ReactJs/IsraTech/nike/src/pages/Home.jsx
+++ ReactJs/IsraTech/nike/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Products from '../components/Products';
+import About_us from '../components/About_us';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <Products />
+      <About_us />
     </div>
   );
 };
