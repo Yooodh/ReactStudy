bc5e0f8719fa98ca875a85bd78a3caae067646ef
Yooodh <eogus2604@hanmail.net>
2024-03-19 14:07:24 +0900

    v0.2

---
+++ ReactJs/IsraTech/vite/tailwind.config.js
@@ -0,0 +1,15 @@
+/* @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src?**/*.(js,jsx,ts,tsx'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ebc7bc',
+        secondary: '#f36437',
+        light: '#f2f1ef',
+        gray: '#74768f',
+      },
+    },
+  },
+  plugins: [],
+};
