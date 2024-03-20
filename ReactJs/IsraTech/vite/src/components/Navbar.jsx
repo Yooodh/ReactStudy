29c0f090bb41d9dc37642363996da2252347cdbd
Yooodh <eogus2604@hanmail.net>
2024-03-20 19:32:14 +0900

    v0.3

--- ReactJs/IsraTech/vite/src/components/Navbar.jsx
+++ ReactJs/IsraTech/vite/src/components/Navbar.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiUser, FiShoppingBag } from 'react-icons/fi';
+import { HiMenuAlt3 } from 'react-icons/hi';
+import { MdClose } from 'react-icons/md';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [dropdown, setDropdown] = useState(false);
+
+  const showDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
+  return (
+    <nav>
+      <div>
+        <div>
+          <div>
+            <div>
+              <h2>Cobalten</h2>
+            </div>
+          </div>
+          <ul>
+            <a href='#'>Blog</a>
+            <a href='#'>Shop</a>
+            <a href='#'>Contact</a>
+            <a href='#'>Products</a>
+          </ul>
+          <div>
+            <button>
+              <FiShoppingBag size={18} />
+            </button>
+            <button>
+              <FiUser size={18} />
+            </button>
+          </div>
+          {dropdown ? (
+            <div onClick={showDropdown}>
+              <MdClose />
+            </div>
+          ) : (
+            <div onClick={showDropdown}>
+              <HiMenuAlt3 />
+            </div>
+          )}
+        </div>
+        {dropdown ? (
+          <div onClick={showDropdown}>
+            <div>
+              <ul>
+                <a href='#'>Blog</a>
+                <a href='#'>Shop</a>
+                <a href='#'>Contact</a>
+                <a href='#'>Products</a>
+              </ul>
+              <div>
+                <button>
+                  <FiShoppingBag size={18} />
+                </button>
+                <button>
+                  <FiUser size={18} />
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
