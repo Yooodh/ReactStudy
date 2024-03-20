042c7fa0beb3f8b59f7b482eef33f77823dfbfe3
Yooodh <eogus2604@hanmail.net>
2024-03-21 00:02:52 +0900

    v0.4

--- ReactJs/IsraTech/vite/src/components/Navbar.jsx
+++ ReactJs/IsraTech/vite/src/components/Navbar.jsx
@@ -11,52 +11,101 @@ const Navbar = () => {
   };
 
   return (
-    <nav>
-      <div>
-        <div>
-          <div>
-            <div>
-              <h2>Cobalten</h2>
+    <nav className='w-full h-24 flex flex-col justify-center items-center lg:bg-transparent bg-whtie lg:relative fixed z-20'>
+      <div className='container mx-atuo lg:px-6'>
+        <div className='lg:w-full w-11/12 mx-auto h-full flex justify-between items-center'>
+          <div className='flex flex-col gap-y-4'>
+            <div className='flex items-center gap-x-2'>
+              <h2 className='font-semibold text-3xl'>Cobalten</h2>
             </div>
           </div>
-          <ul>
-            <a href='#'>Blog</a>
-            <a href='#'>Shop</a>
-            <a href='#'>Contact</a>
-            <a href='#'>Products</a>
+          <ul className='flex-1 flex justify-center items-center xl:gap-12 gap-x-4 max-lg:hidden'>
+            <a
+              href='#'
+              className='leading-normal no-underline text-black font-bold text-lg hover:text-secondary'
+            >
+              Blog
+            </a>
+            <a
+              href='#'
+              className='leading-normal no-underline text-black font-bold text-lg hover:text-secondary'
+            >
+              Shop
+            </a>
+            <a
+              href='#'
+              className='leading-normal no-underline text-black font-bold text-lg hover:text-secondary'
+            >
+              Contact
+            </a>
+            <a
+              href='#'
+              className='leading-normal no-underline text-black font-bold text-lg hover:text-secondary'
+            >
+              Products
+            </a>
           </ul>
-          <div>
-            <button>
+          <div className='flex max-lg:hidden gap-x-4'>
+            <button className='bg-secondary rounded-[10px] p-2 border-none text-whtie cursor-pointer hover:bg-white hover:text-secondary'>
               <FiShoppingBag size={18} />
             </button>
-            <button>
+            <button className='bg-secondary rounded-[10px] p-2 border-none text-whtie cursor-pointer hover:bg-white hover:text-secondary'>
               <FiUser size={18} />
             </button>
           </div>
           {dropdown ? (
-            <div onClick={showDropdown}>
+            <div
+              onClick={showDropdown}
+              className='lg:hidden text-[22px] cursor-pointer text-black'
+            >
               <MdClose />
             </div>
           ) : (
-            <div onClick={showDropdown}>
+            <div
+              onClick={showDropdown}
+              className='lg:hidden text-[22px] cursor-pointer text-black'
+            >
               <HiMenuAlt3 />
             </div>
           )}
         </div>
         {dropdown ? (
-          <div onClick={showDropdown}>
-            <div>
-              <ul>
-                <a href='#'>Blog</a>
-                <a href='#'>Shop</a>
-                <a href='#'>Contact</a>
-                <a href='#'>Products</a>
+          <div
+            onClick={showDropdown}
+            className='lg:hidden w-full h-[100vh] fixed top-24 bg-white ease-in-out duration-100'
+          >
+            <div className='w-full h-[320px] flex flex-col items-baseline pt-8 gap-4'>
+              <ul className='text-center p-0 flex flex-col justify-center w-full gap-y-8'>
+                <a
+                  href='#'
+                  className='leading-normal no-underline text-black font-bold text-lg hover:text-primary'
+                >
+                  Blog
+                </a>
+                <a
+                  href='#'
+                  className='leading-normal no-underline text-black font-bold text-lg hover:text-primary'
+                >
+                  Shop
+                </a>
+                <a
+                  href='#'
+                  className='leading-normal no-underline text-black font-bold text-lg hover:text-primary'
+                >
+                  Contact
+                </a>
+                <a
+                  href='#'
+                  className='leading-normal no-underline text-black font-bold text-lg hover:text-primary'
+                >
+                  Products
+                </a>
               </ul>
-              <div>
-                <button>
+              <div className='flex flex-col justify-center items-center w-full gap-y-8 mt-4'>
+                <button className='bg-secondary rounded-[10px] p-2 border-none text-white cursor-pointer hover:bg-white hover:text-secondary'>
                   <FiShoppingBag size={18} />
                 </button>
-                <button>
+                <button className='bg-secondary rounded-[10px] p-2 border-none text-white cursor-pointer hover:bg-white hover:text-secondary'>
                   <FiUser size={18} />
                 </button>
               </div>
