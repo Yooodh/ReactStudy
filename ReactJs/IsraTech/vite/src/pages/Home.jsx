06c70268611f0dc07d2c25d117597bfd85cb4521
Yooodh <eogus2604@hanmail.net>
2024-03-22 18:00:34 +0900

    v0.5

--- ReactJs/IsraTech/vite/src/pages/Home.jsx
+++ ReactJs/IsraTech/vite/src/pages/Home.jsx
@@ -5,6 +5,42 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        <div></div>
+        <div>
+          <div>
+            <div>
+              <h1>
+                Good Living <br /> Better
+                <span>Life</span>
+              </h1>
+              <p>Find your way to the good living with our furniture.</p>
+              <div>
+                <input type='text' placeholder='Office chair' />
+                <button>Search</button>
+              </div>
+              <div>
+                <span>
+                  <h6>419</h6>
+                  <p>Total Products</p>
+                </span>
+                <span>
+                  <h6>102</h6>
+                  <p>Crafters</p>
+                </span>
+                <span>
+                  <h6>5k</h6>
+                  <p>Relations</p>
+                </span>
+              </div>
+            </div>
+            <div>
+              <div></div>
+              <img src='/chair_image2.png' alt='image of a chair' />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
