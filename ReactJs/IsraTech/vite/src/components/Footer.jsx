66ee3c30cbc974bd6dfb2938ba5876de814f03a6
Yooodh <eogus2604@hanmail.net>
2024-04-07 00:29:38 +0900

    v1.4

--- ReactJs/IsraTech/vite/src/components/Footer.jsx
+++ ReactJs/IsraTech/vite/src/components/Footer.jsx
@@ -3,34 +3,68 @@ import { FaFacebookF, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
+    <footer className='container mx-auto px-6 py-8 bg-white rounded-b-[50px]'>
+      <div className='flex flex-wrap items-end justify-between gap-8'>
+        <div className='lg:w-[60%] flex items-center text-2xl gap-8'>
           <FaTwitter />
           <FaFacebookF />
           <FaGoogle />
           <FaInstagram />
         </div>
-        <div>
-          <div>
-            <h6>Shop</h6>
-            <a href='#'>Chair</a>
-            <a href='#'>Table</a>
-            <a href='#'>Bed</a>
+        <div className='flex justify-between gap-8 md:w-1/3 w-full'>
+          <div className='flex flex-col gap-8'>
+            <h6 className='text-xl font-semibold'>Shop</h6>
+            <a
+              href='#'
+              className='no-underline text-black hover:text-secondary'
+            >
+              Chair
+            </a>
+            <a
+              href='#'
+              className='no-underline text-black hover:text-secondary'
+            >
+              Table
+            </a>
+            <a
+              href='#'
+              className='no-underline text-black hover:text-secondary'
+            >
+              Bed
+            </a>
           </div>
-          <div>
-            <h6>Help</h6>
-            <a href='#'>Contact</a>
-            <a href='#'>Payment</a>
-            <a href='#'>FAQ</a>
+          <div className='flex flex-col gap-8'>
+            <h6 className='text-xl font-semibold'>Help</h6>
+            <a
+              href='#'
+              className='no-underline text-black hover:text-secondary'
+            >
+              Contact
+            </a>
+            <a
+              href='#'
+              className='no-underline text-black hover:text-secondary'
+            >
+              Payment
+            </a>
+            <a
+              href='#'
+              className='no-underline text-black hover:text-secondary'
+            >
+              FAQ
+            </a>
           </div>
         </div>
       </div>
-      <div>
+      <div className='flex flex-wrap gap-4 items-center justify-between mt-16'>
         <p>ⓒ 2020 Cobalten All rights reserved</p>
-        <span>
-          <a href='#'>Privacy policy</a>
-          <a href='#'>Term & conditions</a>
+        <span className='flex justify-between lg:w-1/3 w-full'>
+          <a href='#' className='no-underline text-black hover:text-secondary'>
+            Privacy policy
+          </a>
+          <a href='#' className='no-underline text-black hover:text-secondary'>
+            Term & conditions
+          </a>
         </span>
       </div>
     </footer>
