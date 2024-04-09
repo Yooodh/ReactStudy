5c4b13dadd241790a6ec7b57198405147f46f2da
Yooodh <eogus2604@hanmail.net>
2024-04-09 23:28:24 +0900

    v1.5

--- ReactJs/IsraTech/vite/src/App.js
+++ ReactJs/IsraTech/vite/src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  }, []);
   return (
     <>
       <Home />
