ab4067095dd0bedb56af61dac4745e19b82f4c64
Yooodh <eogus2604@hanmail.net>
2024-04-24 23:48:11 +0900

    v0.2

---
+++ ReactJs/IsraTech/travel/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,te,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6347F9',
+        light: '#FFFFFF1A',
+        gray: '#9DA184',
+        lightGray: '#F4F6FA',
+        orange: '#FA9556',
+      },
+    },
+  },
+  plugins: [],
+};
