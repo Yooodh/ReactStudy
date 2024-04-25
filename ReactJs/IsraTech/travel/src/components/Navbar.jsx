6f4ff3d94ae650cbec7cc5d685886e9865eb2c01
Yooodh <eogus2604@hanmail.net>
2024-04-26 01:27:20 +0900

    v0.4

--- ReactJs/IsraTech/travel/src/components/Navbar.jsx
+++ ReactJs/IsraTech/travel/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { HiMenuAlt3 } from 'react-icons/hi';
 import { MdClose } from 'react-icons/md';
+import logo1 from '../assets/logo1.png';
 
 const Navbar = () => {
   const [dropdown, setDropdown] = useState(false);
@@ -15,7 +16,7 @@ const Navbar = () => {
         <div className='lg:w-full w-11/12 mx-auto h-full flex justify-between items-center'>
           <div className='flex flex-col gap-y-4'>
             <div className='flex items-center gap-x-2'>
-              <img src='' alt='' />
+              <img src={logo1} alt='' className='h-24 ' />
             </div>
           </div>
           <ul className='flex items-center xl:gap-12 gap-x-4 max-lg:hidden'>
