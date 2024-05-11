f458d767cf3b3d94841b50c93de5c00419eafd2a
Yooodh <eogus2604@hanmail.net>
2024-05-12 02:04:25 +0900

    v0.6

--- ReactJs/Webdecoded/portfolio/src/App.js
+++ ReactJs/Webdecoded/portfolio/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Banner from './components/Banner';
+import Contact from './components/Contact';
 import NavBar from './components/NavBar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
