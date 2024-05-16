731ce1829b73d805e7d9411df1033f46f01ece2c
Yooodh <eogus2604@hanmail.net>
2024-05-16 14:17:19 +0900

    v1.0

--- ReactJs/Webdecoded/portfolio/src/components/Footer.jsx
+++ ReactJs/Webdecoded/portfolio/src/components/Footer.jsx
@@ -1,37 +1,37 @@
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import MailchimpForm from './MailchimpForm';
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
-function Footer() {
+const Footer = () => {
   return (
     <footer className='footer'>
       <Container>
-        <Row className='align-item-center'>
+        <Row className='align-items-center'>
           <MailchimpForm />
-          <Col sm={6}>
+          <Col size={12} sm={6}>
             <img src={logo} alt='Logo' />
           </Col>
-          <Col sm={6} className='text-center text-sm-end'>
+          <Col size={12} sm={6} className='text-center text-sm-end'>
             <div className='social-icon'>
-              <a href=''>
-                <img src={navIcon1} alt='' />
+              <a href='#'>
+                <img src={navIcon1} alt='Icon' />
               </a>
-              <a href=''>
-                <img src={navIcon2} alt='' />
+              <a href='#'>
+                <img src={navIcon2} alt='Icon' />
               </a>
-              <a href=''>
-                <img src={navIcon3} alt='' />
+              <a href='#'>
+                <img src={navIcon3} alt='Icon' />
               </a>
             </div>
-            <p>CopyRight 2022. All Right Reserved</p>
+            <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
   );
-}
+};
 
 export default Footer;
