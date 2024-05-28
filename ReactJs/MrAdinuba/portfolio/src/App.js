1d40ff34c0eeb862ae648be631467872723613fd
Yooodh <eogus2604@hanmail.net>
2024-05-29 00:01:46 +0900

    v0.3

--- ReactJs/MrAdinuba/portfolio/src/App.js
+++ ReactJs/MrAdinuba/portfolio/src/App.js
@@ -1,7 +1,25 @@
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Services from './components/Services';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Testimonial from './components/Testimonial';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <>
-     Hello World!!!
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Skills />
+      <Projects />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </>
   );
 }
