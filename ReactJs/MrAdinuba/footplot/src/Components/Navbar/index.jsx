4c0db3ff006f44edff0fef2e994dfa673f77e4dc
Yooodh <eogus2604@hanmail.net>
2024-06-19 00:30:06 +0900

    v0.4

--- ReactJs/MrAdinuba/footplot/src/Components/Navbar/index.jsx
+++ ReactJs/MrAdinuba/footplot/src/Components/Navbar/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Navbar.css';
-import { FaCartShopping } from 'react-icons/fa6';
+import { FaBars, FaCartShopping } from 'react-icons/fa6';
+import Logo from '../../Commons/Logo';
+import { FaTimes } from 'react-icons/fa';
+import { ImSearch } from 'react-icons/im';
 
 const Navbar = () => {
   return (
     <nav className='flex'>
+      <div className='column flex-center'>
+        <Logo />
+      </div>
+      <input type='checkbox' id='sidebar-toggle' />
+      <label htmlFor='sidebar-toggel' className='overlay'></label>
       <div className='column flex-center tabs sidebar'>
+        <label
+          htmlFor='sidebar-toggle'
+          className='flex-center icon-wrapper cancel-btn'
+        >
+          <FaTimes />
+        </label>
         <a href='#' className='tab active'>
           <h4>Home</h4>
         </a>
@@ -24,9 +38,17 @@ const Navbar = () => {
       </div>
       <div className='column flex-center buttons-wrapper'>
         <button className='flex-center btn search-btn'>
+          <ImSearch />
+        </button>
+        <button className='flex-center btn'>
           <FaCartShopping />
         </button>
-        <button className='flex-center btn contact-btn'>Get started</button>
+        <button className='flex-center btn primary contact-btn'>
+          Get started
+        </button>
+        <label htmlFor='sidebar-toggle' className='flex-center btn menu-btn'>
+          <FaBars />
+        </label>
       </div>
     </nav>
   );
