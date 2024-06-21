de9b7cb8deb3f9de63b4b23c4d317eb4113cfb55
Yooodh <eogus2604@hanmail.net>
2024-06-22 00:03:19 +0900

    v0.7

--- ReactJs/MrAdinuba/footplot/src/Components/About/index.jsx
+++ ReactJs/MrAdinuba/footplot/src/Components/About/index.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import './About.css';
+import shoe1 from '../../assets/shoe-1.jpeg';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id='about'>
+      <div className='container'>
+        <div className='column about-image'>
+          <img src={shoe1} alt='' />
+        </div>
+        <div className='column'>
+          <h2 className='sub-title primary'>About Us</h2>
+          <h1 className='heading-1 gradient-text'>
+            Discover High-Quality Shoes Here
+          </h1>
+          <div className='content'>
+            <p className='muted'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+              deserunt. Nulla nam saepe voluptas! Quibusdam soluta nulla
+              perspiciatis delectus, officiis aspernatur aliquid blanditiis
+              recusandae molestiae, labore excepturi libero ipsa nisi!
+            </p>
+            <p className='muted'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+              deserunt. Nulla nam saepe voluptas! Quibusdam soluta nulla
+              perspiciatis delectus, officiis aspernatur aliquid blanditiis
+              recusandae molestiae, labore excepturi libero ipsa nisi!
+            </p>
+          </div>
+          <div className='buttons-wrapper'>
+            <a href='#products' className='btn primary'>
+              Explore more
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
