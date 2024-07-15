54e3cf3eee3a4d68e99618448d58e8f7a03f9302
Yooodh <eogus2604@hanmail.net>
2024-07-16 01:17:40 +0900

    v1.4

--- ReactJs/MrAdinuba/portfolio2/src/Components/Blog/index.jsx
+++ ReactJs/MrAdinuba/portfolio2/src/Components/Blog/index.jsx
@@ -1,8 +1,100 @@
 import React from 'react';
 import './Blog.css';
+import blog1 from '../../assets/blog-1.png';
+import blog2 from '../../assets/blog-2.png';
+import blog3 from '../../assets/blog-3.jpg';
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <section id='blog'>
+      <div className='container'>
+        <div className='section-header'>
+          <h1 className='heading-1'>
+            <span className='gradient-text'>Blogs</span>
+          </h1>
+          <p className='muted'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            eveniet maiores sunt corrupti temporibus cum saepe, unde officiis
+            magni laborum velit? In beatae quos officia iste aperiam repudiandae
+            similique totam!
+          </p>
+        </div>
+        <div className='blogs-container'>
+          {/* blog start */}
+          <div className='blog'>
+            <a href='' className='picture'>
+              <img src={blog1} alt='' />
+            </a>
+            <div className='details'>
+              <h3 className='title clamp-2'>
+                Crafting a User-Centered Portfolio: Best Practices
+              </h3>
+              <p className='muted clamp-2'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquid nihil eligendi porro sit repellendus. Accusantium a
+                ullam impedit adipisci, reiciendis vitae, earum hic distinctio
+                consequuntur eius cum expedita iusto aspernatur.
+              </p>
+            </div>
+            <div className='buttons-wrapper'>
+              <a href='#' className='btn primary'>
+                Read more
+              </a>
+            </div>
+          </div>
+          {/* blog end */}
+
+          {/* blog start */}
+          <div className='blog'>
+            <a href='' className='picture'>
+              <img src={blog2} alt='' />
+            </a>
+            <div className='details'>
+              <h3 className='title clamp-2'>
+                Effective Case Studies: Going Beyond The Surface
+              </h3>
+              <p className='muted clamp-2'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquid nihil eligendi porro sit repellendus. Accusantium a
+                ullam impedit adipisci, reiciendis vitae, earum hic distinctio
+                consequuntur eius cum expedita iusto aspernatur.
+              </p>
+            </div>
+            <div className='buttons-wrapper'>
+              <a href='#' className='btn primary'>
+                Read more
+              </a>
+            </div>
+          </div>
+          {/* blog end */}
+
+          {/* blog start */}
+          <div className='blog'>
+            <a href='#' className='picture'>
+              <img src={blog3} alt='' />
+            </a>
+            <div className='details'>
+              <h3 className='title clamp-2'>
+                Keeping Your Portfolio Updated: A Continuous Process
+              </h3>
+              <p className='muted clamp-2'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquid nihil eligendi porro sit repellendus. Accusantium a
+                ullam impedit adipisci, reiciendis vitae, earum hic distinctio
+                consequuntur eius cum expedita iusto aspernatur.
+              </p>
+            </div>
+            <div className='buttons-wrapper'>
+              <a href='#' className='btn primary'>
+                Read more
+              </a>
+            </div>
+          </div>
+          {/* blog end */}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
