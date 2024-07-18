d7b4c9e831c71783170a8e01f4333b1cfd0ba846
Yooodh <eogus2604@hanmail.net>
2024-07-18 19:41:24 +0900

    v1.6

--- ReactJs/MrAdinuba/portfolio2/src/Components/Footer/index.jsx
+++ ReactJs/MrAdinuba/portfolio2/src/Components/Footer/index.jsx
@@ -1,8 +1,67 @@
 import React from 'react';
 import './Footer.css';
+import Logo from '../Logo';
+import { Link } from 'react-scroll';
+import SocialHandles from '../SocialHandles';
+
+const footerTabs = [
+  {
+    name: 'Home',
+    id: 'hero',
+  },
+  {
+    name: 'About',
+    id: 'about',
+  },
+  {
+    name: 'Skills',
+    id: 'skills',
+  },
+  {
+    name: 'Services',
+    id: 'services',
+  },
+  {
+    name: 'Projects',
+    id: 'projects',
+  },
+  {
+    name: 'Feedbacks',
+    id: 'feedbacks',
+  },
+  {
+    name: 'Blog',
+    id: 'blog',
+  },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className='container'>
+        <Logo />
+        <div className='flex-center footer-tabs'>
+          {footerTabs.map((tab, index) => (
+            <Link
+              className='flex-center tab'
+              to={tab.id}
+              spy={true}
+              smooth={true}
+              key={index}
+            >
+              <h4>{tab.name}</h4>
+            </Link>
+          ))}
+        </div>
+        <SocialHandles />
+      </div>
+      {/* end container */}
+      <div className='flex-center copyright'>
+        <p className='muted'>Build with love by Emmanuel Eze Adinuba</p>
+        <p>Copyright &copy; All right reserved -| 2024</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
