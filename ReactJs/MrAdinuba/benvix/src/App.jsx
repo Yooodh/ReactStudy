89bcc4d8daf1370820ecd59e1349858e678d9d0e
Yooodh <eogus2604@hanmail.net>
2024-07-24 00:19:52 +0900

    v0.3

--- ReactJs/MrAdinuba/benvix/src/App.jsx
+++ ReactJs/MrAdinuba/benvix/src/App.jsx
@@ -1,7 +1,27 @@
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Services from './components/Services';
+import Teams from './components/Teams';
+import Project from './components/Project';
+import Testimonial from './components/Testimonial';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Teams />
+      <Project />
+      <Testimonial />
+      <Blog />
+      <Contact />
+      <Footer />
     </>
   );
 }
