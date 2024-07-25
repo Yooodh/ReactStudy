2e04bcda265568d8b55f5fa9eacc32fd18579ef1
Yooodh <eogus2604@hanmail.net>
2024-07-25 19:41:34 +0900

    v0.4

---
+++ ReactJs/MrAdinuba/benvix/src/components/Logo/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PiBuildingsFill } from 'react-icons/pi';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+      <div className='icon__container'>
+        <PiBuildingsFill />
+      </div>
+      <h1 className='name'>
+        Ben<span>vix</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
--- ReactJs/MrAdinuba/benvix/src/components/Navbar/index.jsx
+++ ReactJs/MrAdinuba/benvix/src/components/Navbar/index.jsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
+import { navTabs } from '../../data';
+import { Link } from 'react-scroll';
+import { FaTimes } from 'react-icons/fa';
+import { RiMenu3Fill } from 'react-icons/ri';
+import Logo from '../Logo';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [visible, setVisible] = useState(false);
+  const handleScroll = () => {
+    const currentScrollPosition = window.scrollY;
+    if (currentScrollPosition > 145) {
+      return setVisible(true);
+    }
+    setVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <nav className={`navbar ${visible ? 'visible' : ''}`}>
+      <Logo />
+      <div className='box nav__tabs'>
+        <div className='icon__container cancel__btn'>
+          <FaTimes />
+        </div>
+        {navTabs.map((tab, index) => (
+          <Link
+            to={tab.id}
+            className='tab'
+            activeClass='active'
+            smooth={true}
+            offset={-60}
+            key={index}
+          >
+            {tab.name}
+          </Link>
+        ))}
+      </div>
+      <div className='box buttons'>
+        <Link to='contact' className='btn contact__btn'>
+          Contact Us
+        </Link>
+        <div className='icon__container menu__btn'>
+          <RiMenu3Fill />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
