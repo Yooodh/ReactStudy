a0d792e649a00145dfd8b7425acaaacbaad840b0
Yooodh <eogus2604@hanmail.net>
2024-07-26 18:20:53 +0900

    v0.5

--- ReactJs/MrAdinuba/benvix/src/components/Header/index.jsx
+++ ReactJs/MrAdinuba/benvix/src/components/Header/index.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-scroll';
+import { hero_image } from '../../assets';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header id='header'>
+      <div className='container full__height'>
+        <div className='row'>
+          <div className='column'>
+            <h1 className='title'>
+              Building Dreams, <span className='g-text'>Creating Reality</span>
+            </h1>
+            <p className='text__muted description'>
+              We are dedicated to turning your visions into tangible
+              masterpieces. With years of experience and a team of skilled
+              professionals, we specialize in delivering high-quality
+              construction services tailored to meet the unique needs of each
+              client.
+            </p>
+            <div className='buttons__container'>
+              <Link
+                to='services'
+                smooth={true}
+                className='btn
+              '
+              >
+                Our Services
+              </Link>
+              <Link
+                to='contact'
+                smooth={true}
+                className='btn btn__primary
+              '
+              >
+                Contact Us
+              </Link>
+            </div>
+          </div>
+          <div className='column'>
+            <div className='image__container'>
+              <img src={hero_image} alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- ReactJs/MrAdinuba/benvix/src/components/Navbar/index.jsx
+++ ReactJs/MrAdinuba/benvix/src/components/Navbar/index.jsx
@@ -5,8 +5,10 @@ import { Link } from 'react-scroll';
 import { FaTimes } from 'react-icons/fa';
 import { RiMenu3Fill } from 'react-icons/ri';
 import Logo from '../Logo';
+import gsap from 'gsap';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const [visible, setVisible] = useState(false);
   const handleScroll = () => {
     const currentScrollPosition = window.scrollY;
@@ -21,11 +23,34 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  useEffect(() => {
+    if (visible) {
+      gsap.fromTo(
+        '.navbar',
+        {
+          Y: -250,
+        },
+        {
+          y: 0,
+          top: 0,
+        }
+      );
+    }
+  }, [visible]);
+
   return (
     <nav className={`navbar ${visible ? 'visible' : ''}`}>
+      {open ? (
+        <div className='sidebar__overlay' onClick={() => setOpen(!open)}></div>
+      ) : (
+        ''
+      )}
       <Logo />
-      <div className='box nav__tabs'>
-        <div className='icon__container cancel__btn'>
+      <div className={`box nav__tabs ${open ? 'open' : ''}`}>
+        <div
+          className='icon__container cancel__btn'
+          onClick={() => setOpen(!open)}
+        >
           <FaTimes />
         </div>
         {navTabs.map((tab, index) => (
@@ -34,7 +59,8 @@ const Navbar = () => {
             className='tab'
             activeClass='active'
             smooth={true}
-            offset={-60}
+            offset={-70}
+            onClick={() => setOpen(false)}
             key={index}
           >
             {tab.name}
@@ -45,7 +71,10 @@ const Navbar = () => {
         <Link to='contact' className='btn contact__btn'>
           Contact Us
         </Link>
-        <div className='icon__container menu__btn'>
+        <div
+          className='icon__container menu__btn'
+          onClick={() => setOpen(!open)}
+        >
           <RiMenu3Fill />
         </div>
       </div>
