f846ed05d69070fabce37919e5bf12249cfb84b7
Yooodh <eogus2604@hanmail.net>
2024-07-29 14:13:26 +0900

    v0.8

--- ReactJs/MrAdinuba/benvix/src/components/Services/index.jsx
+++ ReactJs/MrAdinuba/benvix/src/components/Services/index.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import './Services.css';
+import { services } from '../../data';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section id='services'>
+      <div className='container'>
+        <div className='services__top'>
+          <h1 className='title'>
+            Our <span className='g-text'>Services</span>
+          </h1>
+          <h3 className='sub__title'>
+            We specialize in strategic construction innovation.
+          </h3>
+        </div>
+        <div className='services__container'>
+          {services.map((service, index) => (
+            <ServiceCard
+              icon={service.icon}
+              name={service.name}
+              description={service.description}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
