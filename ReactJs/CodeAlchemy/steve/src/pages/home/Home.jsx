8f3dc95901fcf8d821d606b2fc9f3c3f8fe92c94
Yooodh <eogus2604@hanmail.net>
2025-01-03 21:28:12 +0900

    v0.7

--- ReactJs/CodeAlchemy/steve/src/pages/home/Home.jsx
+++ ReactJs/CodeAlchemy/steve/src/pages/home/Home.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import './home.css';
 
+import Profile from '../../assets/home.jpg';
+
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className='home section grid'>
+      <img src={Profile} alt='' className='home__img' />
+
+      <div className='home__content'>
+        <div className='home__data'>
+          <h1 className='home__title'>
+            <span>I'm Steve Milner.</span> Web Designer
+          </h1>
+
+          <p className='home__description'>
+            I'm a Tunisian based web designer & front-end developer focused on
+            crafting clean & user-friendly experiences, I am passionate about
+            building excellent software that improves the lives of those around
+            me.
+          </p>
+
+          <Link to='/about' className='button'>
+            More About Me{' '}
+            <span className='button__icon'>
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
