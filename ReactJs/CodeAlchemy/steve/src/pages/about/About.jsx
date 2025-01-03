9927733496abcb984401545d7145dfda2e439842
Yooodh <eogus2604@hanmail.net>
2025-01-04 05:10:54 +0900

    v0.8

--- ReactJs/CodeAlchemy/steve/src/pages/about/About.jsx
+++ ReactJs/CodeAlchemy/steve/src/pages/about/About.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './about.css';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main className='section container'>
+      <section className='about'>
+        <h2 className='section__title'>
+          About <span>Me</span>
+        </h2>
+      </section>
+    </main>
+  );
 };
 
 export default About;
