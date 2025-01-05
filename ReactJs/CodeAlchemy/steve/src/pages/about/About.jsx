5bd9f08e1596aa07b4234aacdac05d5d3069267e
Yooodh <eogus2604@hanmail.net>
2025-01-05 18:05:51 +0900

    v0.9

--- ReactJs/CodeAlchemy/steve/src/pages/about/About.jsx
+++ ReactJs/CodeAlchemy/steve/src/pages/about/About.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './about.css';
 
+import Info from '../../components/Info';
+import Stats from '../../components/Stats';
+import { FaDownload } from 'react-icons/fa';
+import CV from '../../assets/steve-Cv.pdf';
+
 const About = () => {
   return (
     <main className='section container'>
@@ -8,6 +13,25 @@ const About = () => {
         <h2 className='section__title'>
           About <span>Me</span>
         </h2>
+
+        <div className='about__container grid'>
+          <div className='about__info'>
+            <h3 className='section__subtitle'>Personal Infos</h3>
+            <ul className='info__list grid'>
+              <Info />
+            </ul>
+            <a href={CV} download='' className='button'>
+              Download CV
+              <span className='button__icon'>
+                <FaDownload />
+              </span>
+            </a>
+          </div>
+
+          <div className='stats grid'>
+            <Stats />
+          </div>
+        </div>
       </section>
     </main>
   );
