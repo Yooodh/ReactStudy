2d5e8ea1691bb66ceaaa20ddd19dacb55ddb1879
Yooodh <eogus2604@hanmail.net>
2025-01-27 18:39:39 +0900

    v0.7

--- ReactJs/CodeAlchemy/coffee/src/App.jsx
+++ ReactJs/CodeAlchemy/coffee/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Features from './components/features/Features';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Features />
     </>
   );
 }
