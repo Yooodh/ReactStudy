5fdaa52fbb6db613999f9e81df165668862751da
Yooodh <eogus2604@hanmail.net>
2025-03-12 00:08:09 +0900

    v0.8

--- ReactJs/ZainKeepScode/nft/app/_components/hero-section/hero-section.tsx
+++ ReactJs/ZainKeepScode/nft/app/_components/hero-section/hero-section.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroFrame from './hero-frame';
 import HeroMovingFrames from './hero-moving-frames';
 import Stats from './stats';
+import TextAnimation from '../text-animation';
 
 const HeroSection = () => {
   return (
@@ -15,7 +16,9 @@ const HeroSection = () => {
       <div className='text-25 lg:text-60 flex flex-col items-center justify-center gap-[14px] font-bold lg:py-[25px]'>
         {/* first line */}
         <div className='flex items-center gap-[15px]'>
-          <span>Discover</span>
+          <TextAnimation delay={0.8} divideBy='letter'>
+            Discover
+          </TextAnimation>
           <HeroFrame
             height={{
               base: 34,
@@ -28,7 +31,9 @@ const HeroSection = () => {
             bgImage='/images/hero-frame-1.jpg'
             animationType='scaleDown'
           />
-          <span>Collect</span>
+          <TextAnimation delay={0.8} divideBy='letter'>
+            Collect
+          </TextAnimation>
         </div>
 
         {/* second line */}
@@ -46,11 +51,15 @@ const HeroSection = () => {
             animationType='move'
           />
 
-          <span>& Sell Extraordinary</span>
+          <TextAnimation delay={1.1} divideBy='letter'>
+            & Sell Extraordinary
+          </TextAnimation>
         </div>
 
         {/* third line */}
-        <span>Rare NFTs</span>
+        <TextAnimation delay={1} divideBy='letter'>
+          Rare NFTs
+        </TextAnimation>
       </div>
 
       <div className='flex flex-col items-center justify-center gap-[25px] lg:flex-row lg:items-start lg:justify-between '>
@@ -66,10 +75,12 @@ const HeroSection = () => {
 
         {/* small info */}
         <span className='flex w-[202px] items-center justify-center text-center text-[7px] leading-3 lg:order-1 lg:text-start lg:leading-6 lg:text-14'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic vitae,
-          perferendis, provident illum cum voluptatibus aliquam praesentium
-          dolores accusantium porro quia iure assumenda quam consequuntur esse
-          nam. Natus, repellendus reiciendis.
+          <TextAnimation delay={1} divideBy='word'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic vitae,
+            perferendis, provident illum cum voluptatibus aliquam praesentium
+            dolores accusantium porro quia iure assumenda quam consequuntur esse
+            nam. Natus, repellendus reiciendis.
+          </TextAnimation>
         </span>
       </div>
     </div>
