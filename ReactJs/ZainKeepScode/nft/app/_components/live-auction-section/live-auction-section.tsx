f0740aa72149e34c88bafa1e5ff13a26ce650d5e
Yooodh <eogus2604@hanmail.net>
2025-03-14 02:13:51 +0900

    v1.0

--- ReactJs/ZainKeepScode/nft/app/_components/live-auction-section/live-auction-section.tsx
+++ ReactJs/ZainKeepScode/nft/app/_components/live-auction-section/live-auction-section.tsx
@@ -1,26 +1,46 @@
+'use client';
 import Image from 'next/image';
-import React from 'react';
+import React, { useRef } from 'react';
 import TextAnimation from '../text-animation';
+import Button from '../button';
+import { useScroll, useTransform, motion } from 'framer-motion';
 
 const LiveAuctionSection = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['0 1', '1 0'],
+  });
+
+  const monkeyY = useTransform(scrollYProgress, [0, 0.7], [300, 0]);
+
   return (
-    <div className='flex flex-col gap-[40px]'>
+    <div className='flex flex-col gap-[40px] lg:flex-row lg:gap-[89px]'>
       {/* monkey frame */}
-      <div className='relative h-[243px] rounded-lg overflow-hidden bg-gradient-to-b from-[#EF962E] to-[#FFAF36]'>
-        <Image
-          src='/images/black-monkey.png'
-          alt='monkey'
-          fill
-          className='object-cover'
-        />
+      <div
+        ref={containerRef}
+        className='relative h-[243px] rounded-lg overflow-hidden bg-gradient-to-b from-[#EF962E] to-[#FFAF36] lg:h-[601px] lg:flex-1 lg:rounded-3xl'
+      >
+        <motion.div
+          style={{ y: monkeyY }}
+          className='relative h-[100%] w-[100%] bottom-0 left-0 right-0 ml-auto mr-auto'
+        >
+          <Image
+            src='/images/black-monkey.png'
+            alt='monkey'
+            fill
+            className='object-cover'
+          />
+        </motion.div>
       </div>
 
       {/* live auction details */}
-      <div className='flex flex-col gap-[14px]'>
+      <div className='flex flex-col gap-[14px] lg:flex-2 lg:justify-between'>
         {/* live auction */}
         <div className='flex gap-[12px]'>
-          <div className='h-auto w-[4px] rounded-md bg-[#6351DB]' />
-          <span className='text-[12px]'>
+          <div className='h-auto w-[4px] lg:w-[12px] rounded-md bg-[#6351DB]' />
+          <span className='text-[12px] lg:text-25'>
             <TextAnimation divideBy='word' delay={0.3}>
               Live Auction
             </TextAnimation>
@@ -28,7 +48,7 @@ const LiveAuctionSection = () => {
         </div>
 
         {/* heading */}
-        <span className='text-30 font-bold'>
+        <span className='text-30 font-bold lg:text-60 lg:mt-[40px]'>
           <TextAnimation delay={0.4} divideBy='word'>
             Bored_Ape #333
           </TextAnimation>
@@ -48,6 +68,18 @@ const LiveAuctionSection = () => {
             name='Your Dream'
           />
         </div>
+
+        {/* bid blocks */}
+        <div className='flex justify-between gap-[20px]'>
+          <BidBlock title='Current Bid' content='1.23 ETH' />
+          <BidBlock title='Ends in' content='12 : 50 : 23' />
+        </div>
+
+        <div className='flex gap-[20px] pt-[10px]'>
+          <Button>Start Mining</Button>
+
+          <Button variant='outlined'>Learn more</Button>
+        </div>
       </div>
     </div>
   );
@@ -64,22 +96,39 @@ const Block = ({
 }) => {
   return (
     <div className='flex flex-1 flex-col gap-[14px]'>
-      <span className='text-14'>
+      <span className='text-14 lg:text-25'>
         <TextAnimation delay={0.5} divideBy='word'>
           {title}
         </TextAnimation>
       </span>
 
-      <div className='flex gap-[14px] rounded-lg bg-[#454448] px-[20px] py-[11px]'>
-        <div className='relative h-[25px] w-[25px]'>
+      <div className='flex gap-[14px] rounded-lg bg-[#454448] px-[20px] py-[11px] lg:px-[20px] lg:py-[16px]'>
+        <div className='relative h-[25px] w-[25px] lg:h-[40px] lg:w-[40px]'>
           <Image src={image} alt={'3d avatar'} fill className='rounded-full' />
         </div>
-        <span className='text-14'>
+        <span className='text-14 lg:text-25'>
           <TextAnimation delay={0.5} divideBy='word'>
             {name}
           </TextAnimation>
         </span>
       </div>
+    </div>
+  );
+};
+
+const BidBlock = ({ title, content }: { title: string; content: string }) => {
+  return (
+    <div className='flex flex-1 flex-col'>
+      <span className='text-14 lg:text-25'>
+        <TextAnimation delay={0.5} divideBy='word'>
+          {title}
+        </TextAnimation>
+      </span>
+      <span className='text-27 font-bold lg:text-55'>
+        <TextAnimation delay={0.5} divideBy='word'>
+          {content}
+        </TextAnimation>
+      </span>
     </div>
   );
 };
