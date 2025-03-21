8ed90bd115162ba5d5c6bb265558dcfadada0230
Yooodh <eogus2604@hanmail.net>
2025-03-22 00:12:45 +0900

    v1,8

--- ReactJs/ZainKeepScode/nft/app/layout.tsx
+++ ReactJs/ZainKeepScode/nft/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inria_Sans } from 'next/font/google';
 import type { Metadata } from 'next';
 import Header from './_components/header/header';
 import Footer from './_components/footer/footer';
+import SmoothScrolling from './_components/smooth-scrolling';
 
 const inriaSans = Inria_Sans({
   variable: '--font-inria-sans',
@@ -25,9 +26,17 @@ export default function RootLayout({
       <body
         className={`${inriaSans.variable} relative mx-auto box-border max-w-[1728px] px-[17px] py-[10px] antialiased lg:px-[100px] lg:py-[23px]`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <SmoothScrolling>
+          {/* bg blur */}
+          <div className='absolute left-0 hidden lg:block h-[400px] w-[400px] bg-white opacity-10 blur-3xl lg:-top-[100px]' />
+          <Header />
+
+          {/* bg blur */}
+
+          <div className='absolute -right-0 hidden lg:block h-[400px] w-[400px] bg-white opacity-5 blur-3xl lg:top-[700px]' />
+          {children}
+          <Footer />
+        </SmoothScrolling>
       </body>
     </html>
   );
