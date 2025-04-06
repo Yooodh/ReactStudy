b8081cf2a1595e2107f1afb915d9570c693bcc79
Yooodh <eogus2604@hanmail.net>
2025-04-07 01:00:53 +0900

    v0.3

--- NextJs/next-ppak-todo/app/api/todos/[slug]/route.ts
+++ NextJs/next-ppak-todo/app/api/todos/[slug]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+// 할일 단일 조회
 export async function GET(
   request: NextRequest,
   { params }: { params: { slug: string } }
@@ -20,5 +21,59 @@ export async function GET(
     },
   };
 
+  return NextResponse.json(response, { status: 200 });
+}
+
+// 할일 단일 삭제 (id 존재해야함)
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  // const searchParams = request.nextUrl.searchParams;
+
+  // const search = searchParams.get('search');
+  // const query = searchParams.get('query');
+
+  const response = {
+    message: '단일 할일 삭제 성공!',
+    data: {
+      id: params.slug,
+      title: '오늘도 빡코딩!',
+      is_done: false,
+      // query: query,
+      // query,
+    },
+  };
+
+  // 데이터가 없다 -> 보통 204
+  return NextResponse.json(response, { status: 200 });
+}
+
+// 할일 단일 수정 (id 존재해야함)
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  // const searchParams = request.nextUrl.searchParams;
+
+  // const search = searchParams.get('search');
+  // const query = searchParams.get('query');
+
+  const { title, is_done } = await request.json();
+
+  const editedTodo = {
+    id: params.slug,
+    // key와 data가 같으면 빼도 된다
+    // title:title,
+    title,
+    is_done,
+  };
+
+  const response = {
+    message: '단일 할일 수정 성공!',
+    data: editedTodo,
+  };
+
+  // 데이터가 없다 -> 보통 204
   return NextResponse.json(response, { status: 200 });
 }
--- NextJs/next-ppak-todo/app/api/todos/route.ts
+++ NextJs/next-ppak-todo/app/api/todos/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import dummyTodos from '@/data/dummy.json';
 
+// 모든 할일 가져오기
 export async function GET(request: NextRequest) {
   const response = {
     message: 'todos 몽땅 가져오기',
@@ -8,4 +9,34 @@ export async function GET(request: NextRequest) {
   };
 
   return NextResponse.json(response, { status: 200 });
+}
+
+// 할일 추가
+export async function POST(request: NextRequest) {
+  // const res =await fetch('https://data.mongodb-api.com/...', {
+  //   methud: 'POST',
+  //   headers: {
+  //     'Context-Type': 'application/json',
+  //     'API-Key' : process.env.DATA_API_KEY,
+  //   },
+  //   body: JSON.stringify({time: new Date().toISOString()}),
+  // })
+
+  const { title } = await request.json();
+
+  const newTodo = {
+    id: '10',
+    // key와 data가 같으면 빼도 된다
+    // title:title,
+    title,
+    is_done: false,
+  };
+
+  const response = {
+    message: '할일 추가 성공!',
+    data: newTodo,
+  };
+
+  // 새로 만든 staus는 보통 201
+  return Response.json(response, { status: 201 });
 }
