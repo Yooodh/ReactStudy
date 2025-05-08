56749f33836f00f74f84773f98cf52f42971f50b
Yooodh <eogus2604@hanmail.net>
2025-05-09 00:08:40 +0900

    feat: SearchManufacturer 컴포넌트 생성

---
+++ NextJs/car_showcase/app/components/SearchManufacturer.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { SearchManufacturerProps } from "@/types";
+import React, { useState } from "react";
+
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  Transition,
+} from "@headlessui/react";
+import Image from "next/image";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <ComboboxButton className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </ComboboxButton>
+
+          <ComboboxInput
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
