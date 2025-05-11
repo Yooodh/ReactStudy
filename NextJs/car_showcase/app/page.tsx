a5b522dda7047b31dc391a44df482618baef14f9
Yooodh <eogus2604@hanmail.net>
2025-05-12 01:04:34 +0900

    feat: Home 페이지에 리스트 렌더링 및 예외 처리 로직 추가

--- NextJs/car_showcase/app/page.tsx
+++ NextJs/car_showcase/app/page.tsx
@@ -3,9 +3,12 @@ import Hero from "./components/Hero";
 import CustomFilter from "./components/CustomFilter";
 import SearchBar from "./components/SearchBar";
 import { fetchCars } from "@/utils";
+import CarCard from "./components/CarCard";
 
 export default async function Home() {
   const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   console.log(allCars);
 
@@ -27,6 +30,21 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
