f4bcb8ba99143dce81816d45ba0c524eefb35fb3
Yooodh <eogus2604@hanmail.net>
2025-05-14 00:09:10 +0900

    chore: CarDetails 컴포넌트 export 설정

--- NextJs/car_showcase/app/components/index.ts
+++ NextJs/car_showcase/app/components/index.ts
@@ -1,11 +1,12 @@
-import Hero from "./Hero";
-import CustomButton from "./CustomButton";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import SearchBar from "./SearchBar";
-import CustomFilter from "./CustomFilter";
-import SearchManufacturer from "./SearchManufacturer";
-import CarCard from "./CarCard";
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 
 export {
   Hero,
@@ -16,4 +17,5 @@ export {
   CustomFilter,
   SearchManufacturer,
   CarCard,
+  CarDetails,
 };
